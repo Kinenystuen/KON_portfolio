1518e6cf7052e56119e727608a60c97269468fbe
Kinenystuen <Kine_on1@hotmail.com>
2024-11-24 14:09:40 +0100

    MenuBtn

---
+++ src/components/shared/header/MenuBtn.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./HeaderNav.css";
+
+interface HamburgerMenuProps {
+  isOpen: boolean;
+  toggleMenu: () => void;
+}
+
+const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
+  isOpen,
+  toggleMenu
+}) => {
+  return (
+    <div className="hamburger_icon_container flex justify-center items-center">
+      <button
+        className={`hamburger_icon border-none cursor-pointer 
+        rounded-sm focus:outline-none focus:ring-2 ${isOpen ? "cross" : ""}`}
+        aria-expanded={isOpen}
+        title="Navigation menu button"
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        <div className="hLine"></div>
+      </button>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
