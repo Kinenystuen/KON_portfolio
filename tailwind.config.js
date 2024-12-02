14c8cf270d039a5fdccf06d01f8bd0fe65d47f23
Kinenystuen <Kine_on1@hotmail.com>
2024-12-02 13:18:42 +0100

    tw fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,54 +34,53 @@ export default {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)"
-      }
-    },
-    screens: {
-      xs: "375px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px"
-    },
-    container: {
-      center: "true",
-      padding: "1rem"
-    },
-    scale: {
-      ...require("tailwindcss/defaultTheme").scale,
-      115: "1.15",
-      175: "1.75",
-      200: "2",
-      300: "3"
-    },
-    animation: {
-      zoom: "zoom 0.6s ease-in-out",
-      fadeInScale: "fadeInScale 0.5s ease-out forwards",
-      appear: "fadeIn 0.5s ease-out forwards"
-    },
-    keyframes: {
-      zoom: {
-        from: {
-          transform: "scale(0)"
-        },
-        to: {
-          transform: "scale(1)"
-        }
       },
-      fadeInScale: {
-        "0%": {
-          opacity: "0",
-          transform: "scale(0.5)",
-          transformOrigin: "center center"
+      screens: {
+        xs: "375px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px"
+      },
+      container: {
+        center: true,
+        padding: "1rem"
+      },
+      scale: {
+        ...require("tailwindcss/defaultTheme").scale,
+        115: "1.15",
+        175: "1.75",
+        200: "2",
+        300: "3"
+      },
+      animation: {
+        zoom: "zoom 0.6s ease-in-out",
+        fadeInScale: "fadeInScale 0.5s ease-out forwards",
+        appear: "fadeIn 0.5s ease-out forwards"
+      },
+      keyframes: {
+        zoom: {
+          from: {
+            transform: "scale(0)"
+          },
+          to: {
+            transform: "scale(1)"
+          }
         },
-        "100%": {
-          opacity: "1",
-          transform: "scale(1)"
+        fadeInScale: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(0.5)",
+            transformOrigin: "center center"
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1)"
+          }
         }
       }
     }
   },
-  plugins: [require("tailwindcss-animate")],
-  plugins: [require("tailwindcss-motion")]
+  plugins: [require("tailwindcss-animate"), require("tailwindcss-motion")]
 };
