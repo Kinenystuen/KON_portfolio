4692538d400d934c66fe9ea832d7c26112d4c2e1
Kinenystuen <Kine_on1@hotmail.com>
2024-12-02 12:01:02 +0100

    Constants and types

---
+++ src/library/constants/index.ts
@@ -0,0 +1,16 @@
+import { faHome, faContactBook } from "@fortawesome/free-solid-svg-icons";
+
+export const headerNavLinks = [
+  {
+    icon: faHome,
+    title: "Home",
+    route: "/",
+    label: "Home"
+  },
+  {
+    icon: faContactBook,
+    title: "Contact",
+    route: "/contact",
+    label: "Contact"
+  }
+];
---
+++ src/library/types/index.ts
@@ -0,0 +1,37 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+export type INavLink = {
+  icon: IconDefinition;
+  title: string;
+  route: string;
+  label: string;
+};
+
+/* Typographic props */
+export type H1Props = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+/* Button props */
+export type ButtonProps = {
+  ButtonType?: "primary" | "secondary";
+  onClick: () => void;
+  children: React.ReactNode;
+  className?: string;
+  title?: string;
+  ariaLabel?: string;
+};
+
+/* Loader Props */
+export interface LoaderProps {
+  theme?: "light" | "dark";
+}
+
+export interface Gallery {
+  id: string;
+  image: {
+    url: string;
+    alt: string;
+  };
+}
