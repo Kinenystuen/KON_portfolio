be279096cce690cbed056351231a0505cce19d11
Kinenystuen <Kine_on1@hotmail.com>
2024-12-02 13:33:52 +0100

    Vite base to work on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src")
+      }
+    },
+    base: mode === "github" ? "/KON_portfolio/" : "/"
+  };
 });
