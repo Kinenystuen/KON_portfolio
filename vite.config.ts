54ca863d4ff33bd6e3e7bf60f7f011abee973c18
Kinenystuen <Kine_on1@hotmail.com>
2024-12-02 13:50:21 +0100

    Vite base update

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src")
-      }
-    },
-    base: mode === "github" ? "/KON_portfolio/" : "/"
-  };
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+  base: "/KON_portfolio/"
 });
