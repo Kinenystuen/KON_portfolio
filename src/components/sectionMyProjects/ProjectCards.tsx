a606a4a4af2bc563d1c555cfd4b6b2899a5b46dc
Kinenystuen <Kine_on1@hotmail.com>
2024-12-17 18:19:01 +0100

    New project cards design

--- src/components/sectionMyProjects/ProjectCards.tsx
+++ src/components/sectionMyProjects/ProjectCards.tsx
@@ -22,7 +22,7 @@ const ProjectCards: React.FC<ProjectCardsProps> = ({ isInView }) => {
   return (
     <FetchProjects>
       {(data) => (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {data.map((project, index) => (
             <div
               key={project.id}
