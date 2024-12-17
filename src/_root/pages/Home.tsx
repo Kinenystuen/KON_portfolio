900d32a95be4bd4532043e3a07b4da9c147ee9fc
Kinenystuen <Kine_on1@hotmail.com>
2024-12-17 18:16:41 +0100

    New home setup with animated header

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,18 +1,21 @@
 import InterestsSection from "../../components/interestsSection/InterestsSection";
 import KnowledgeSection from "../../components/knowledgeSection/KnowledgeSection";
 import MyProjects from "../../components/sectionMyProjects/MyProjects";
-import HeroSection from "../../components/headerHero/HeroSection";
+import AnimatedHeader from "./AnimatedHeader";
 
 const Home = () => {
   return (
     <div className="relative ">
-      {/* Hero Section */}
-      <HeroSection />
-
-      {/* BODY SECTION */}
-      <MyProjects />
-      <KnowledgeSection />
-      <InterestsSection />
+      <div className=" z-0">
+        {/* Animated header Section */}
+        <AnimatedHeader />
+      </div>
+      <div className="relative z-10 bg-customBg dark:bg-customBgDark">
+        {/* BODY SECTION */}
+        <MyProjects />
+        <KnowledgeSection />
+        <InterestsSection />
+      </div>
     </div>
   );
 };
