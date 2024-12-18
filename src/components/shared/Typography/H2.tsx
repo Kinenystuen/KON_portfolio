8c84cc958ca57b9da1ea8e5b4818d366ea88c7a3
Kinenystuen <Kine_on1@hotmail.com>
2024-12-18 17:05:36 +0100

    updated

--- src/components/shared/Typography/H2.tsx
+++ src/components/shared/Typography/H2.tsx
@@ -3,7 +3,7 @@ import { H1Props } from "../../../library/types";
 function H2({ className, children }: H1Props) {
   return (
     <h2
-      className={`font-header text-2xl px-4 font-semibold text-black dark:text-whiteFont ${className}`}
+      className={`font-header text-2xl md:text-3xl px-4 font-semibold text-black dark:text-whiteFont ${className}`}
     >
       {children}
     </h2>
