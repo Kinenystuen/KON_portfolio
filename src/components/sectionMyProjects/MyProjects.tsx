71323e316e0fb4d21b6a39ca2f9a6894e3ed4cdc
Kinenystuen <Kine_on1@hotmail.com>
2024-12-18 17:04:10 +0100

    Width and text size fix

--- src/components/sectionMyProjects/MyProjects.tsx
+++ src/components/sectionMyProjects/MyProjects.tsx
@@ -7,8 +7,10 @@ const MyProjects = () => {
   return (
     <section ref={sectionRef} className="py-20">
       <div className="container mx-auto py-10 max-w-6xl ">
-        <H2 className="font-bold text-3xl uppercase mb-6">My Projects</H2>
-        <ProjectCards isInView={isSectionInView} />
+        <H2 className="font-bold uppercase mb-6">My Projects</H2>
+        <div className="container mx-auto max-w-4xl lg:max-w-6xl">
+          <ProjectCards isInView={isSectionInView} />
+        </div>
       </div>
     </section>
   );
