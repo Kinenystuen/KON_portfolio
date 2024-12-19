aac5527ce7d4a90f95e35a1f09d42aca44f15c1d
Kinenystuen <Kine_on1@hotmail.com>
2024-12-19 15:15:24 +0100

    Contact Form elements

---
+++ src/components/contact/ContactSection.tsx
@@ -0,0 +1,12 @@
+import Section from "../shared/Section";
+import Contact from "./Contact";
+
+const ContactSection = () => {
+  return (
+    <Section className="mx-auto py-20 my-20 p-2 bg-color4 dark:bg-customBgDark-700">
+      <Contact />
+    </Section>
+  );
+};
+
+export default ContactSection;
