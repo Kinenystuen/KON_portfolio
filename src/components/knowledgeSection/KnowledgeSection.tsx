fcd0e3de6c528e90ade0dd6633ec916e6b2cba9d
Kinenystuen <Kine_on1@hotmail.com>
2024-12-22 15:51:18 +0100

    New colors

--- src/components/knowledgeSection/KnowledgeSection.tsx
+++ src/components/knowledgeSection/KnowledgeSection.tsx
@@ -10,7 +10,7 @@ const KnowledgeSection = () => {
   return (
     <section ref={sectionRef} className="relative">
       <TopWave />
-      <div className="relative bg-gradient-to-r from-color4-400 to-color4-500 dark:from-[#1e485c] dark:to-[#182d38] background-animation">
+      <div className="relative bg-gradient-to-r from-color4-400 to-color4-500 dark:from-violet-900 dark:to-violet-950 background-animation">
         <div className="container mx-auto py-10 max-w-6xl">
           <H2 className="font-bold text-white uppercase mb-6 animate-fadeIn">
             Knowledge
