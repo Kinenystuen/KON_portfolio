58262f2b1114c27da73ca9883bf1431fe0b03a48
Kinenystuen <Kine_on1@hotmail.com>
2024-12-22 15:50:22 +0100

    Added about section and new background

--- src/_root/pages/AnimatedHeader.tsx
+++ src/_root/pages/AnimatedHeader.tsx
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { getBaseUrl } from "../../components/shared/BaseNameUtils";
 import H1 from "../../components/shared/Typography/H1";
+import P from "../../components/shared/Typography/P";
+import useInView from "../../hooks/useInView";
+import { motion } from "framer-motion";
 
 const AnimatedHeader: React.FC = () => {
   const [scrollY, setScrollY] = useState<number>(0);
   const [H1Top, setH1Top] = useState<number>(1.5);
   const [PTop, setPTop] = useState<number>(1.3);
 
+  const { elementRef, isInView } = useInView(0.2);
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  if (isInView && !hasAnimated) {
+    setHasAnimated(true); // Mark animation as completed
+  }
+
   useEffect(() => {
     const handleScroll = () => setScrollY(window.scrollY);
 
     const updateHero = () => {
-      if (window.innerHeight < 600) {
+      if (window.innerHeight < 300) {
         setH1Top(1.1);
         setPTop(1.8);
+      } else if (window.innerHeight < 600) {
+        setH1Top(1.4);
+        setPTop(1.7);
       } else if (window.innerHeight < 800) {
         setH1Top(1.3);
         setPTop(1.8);
@@ -77,7 +90,7 @@ const AnimatedHeader: React.FC = () => {
   const { bounceScale, translateY, scale2 } = calculateBounceTransform();
 
   return (
-    <div className="relative w-full h-[200vh] overflow-hidden">
+    <div className="relative w-full h-[250vh] overflow-hidden">
       <H1
         className="absolute left-1/2 text-2xl  sm:text-2xl md:text-4xl font-bold text-center transition-all duration-300 ease-out z-10"
         style={{
@@ -119,17 +132,24 @@ const AnimatedHeader: React.FC = () => {
           </div>
 
           {/* Background Image */}
-          {/* <div className="h-screen w-screen bg-gradient-to-br from-color4-300 flex justify-center content-center"></div> */}
-          <img
-            src={getBaseUrl("assets/hero_sketch2.png")}
-            // src="https://img.freepik.com/free-vector/hand-drawn-web-design-concept_23-2147839745.jpg?w=360"
-            alt="Zoomed"
-            className="absolute top-0 left-0 w-full h-screen object-cover object-bottom"
-          />
+
+          <div className="relative w-full h-screen">
+            {/* Image */}
+            <img
+              src={getBaseUrl("assets/hero_color.jpg")}
+              alt="Zoomed"
+              className="absolute top-0 left-0 w-full h-screen object-cover object-bottom"
+            />
+
+            {/* Overlay */}
+            <div
+              className={`absolute inset-0 dark:bg-black dark:opacity-40 animate duration-700 ${isInView ? "dark:opacity-80" : ""} pointer-events-none`}
+            ></div>
+          </div>
         </div>
       </div>
       <p
-        className="absolute left-1/2 text-center text-lg font-bold mt-4 transition-all duration-300 ease-out motion-scale-in-[0.45] motion-duration-[1.50s]/scale motion-delay-[0.00s]/scale"
+        className="absolute left-1/2 text-center text-lg font-bold mt-4 transition-all duration-300 ease-out motion-scale-in-[1] motion-duration-[1.50s]/scale motion-delay-[0.00s]/scale"
         style={{
           top: `calc(${maskSize.height} + ${maskSize.height} / ${PTop} )`,
           transform: `translate(-50%, -${scrollY}px)`,
@@ -138,6 +158,47 @@ const AnimatedHeader: React.FC = () => {
       >
         Front-end Developer.
       </p>
+
+      {/* About text? */}
+      <motion.div
+        ref={elementRef}
+        initial={{ opacity: 0, y: 50 }}
+        animate={isInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 0.6, ease: "easeIn" }}
+        className="absolute bottom-[40vh] w-full z-20"
+      >
+        <div className=" mx-3 sm:mx-auto h-[50vh] max-w-4xl flex flex-col md:flex-row items-center justify-center gap-6 p-6 rounded-xl">
+          {/* Profile Image */}
+          <div className="overflow-hidden rounded-full w-56 h-56 shadow-lg flex-shrink-0">
+            <img
+              className="h-full w-full object-cover transition-transform duration-300 ease-out scale-150"
+              src="/assets/img/IMG_0829Kine2.jpg"
+              alt="Profile"
+            />
+          </div>
+
+          {/* Text Section */}
+          <div className="flex-1 flex flex-col text-center md:text-start space-y-2">
+            {/* Greeting */}
+            <P className="text-lg font-semibold text-gray-700 dark:text-gray-300">
+              Hi there!
+            </P>
+            {/* Introduction */}
+            <P className="font-header text-3xl md:text-4xl font-bold leading-tight text-gray-800 dark:text-gray-100">
+              I'm
+              <span className="text-violet-600 dark:text-violet-300 ms-2">
+                Kine
+              </span>
+              , a front-end developer and artist passionate about crafting
+              <span className="text-violet-600 dark:text-violet-400">
+                {" "}
+                beautiful websites.
+              </span>
+            </P>
+          </div>
+        </div>
+      </motion.div>
+
       {/* Svg wave at bottom */}
       <div className="absolute bottom-0 left-0 w-screen">
         <svg
