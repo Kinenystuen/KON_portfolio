c474fa34c262307f45fe1e612ff52af298727146
Kinenystuen <kine_on@hotmail.com>
2025-03-17 22:31:27 +0100

    Remove unused Button component import from FooterBar

--- src/components/shared/FooterBar.tsx
+++ src/components/shared/FooterBar.tsx
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { socialMedia } from "../../library/constants";
 import { Link } from "react-router-dom";
-import Button from "./Button/Button";
 
 const FooterBar = () => {
   return (
