0690e3b589a46954c8d450fa4804ac139ef458eb
Kinenystuen <kine_on@hotmail.com>
2025-04-06 17:16:55 +0200

    Replace AnimatedHeader with HeaderHero component in Home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -2,14 +2,15 @@ import ContactSection from "../../components/contact/ThisContactSection";
 import InterestsSection from "../../components/interestsSection/InterestsSection";
 import KnowledgeSection from "../../components/knowledgeSection/KnowledgeSection";
 import MyProjects from "../../components/sectionMyProjects/MyProjects";
-import AnimatedHeader from "./AnimatedHeader";
+import HeaderHero from "./HeaderHero";
 
 const Home = () => {
   return (
     <div className="relative ">
       <div className=" z-0">
-        {/* Animated header Section */}
-        <AnimatedHeader />
+        {/* Header hero Section */}
+        <HeaderHero />
+        {/* <AnimatedHeader /> */}
       </div>
       <div className="relative z-10 bg-customBg pb-10 dark:bg-customBgDark">
         {/* BODY SECTION */}
